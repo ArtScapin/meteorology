dc11cbd581277ddc958bbe8b46538c8bd41df4bd
ArtScapin <arthurscapin19@gmail.com>
2023-01-01 12:32:28 -0300

    Loading Component

---
+++ src/components/content/Loading.js
@@ -0,0 +1,12 @@
+import style from "./Loading.module.css";
+
+function Loading() {
+  return (
+    <div className={style.Loading}>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Loading;
